0d4486d6dcd3d2ba3db91fdb11103ade22dd256a
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-23 13:51:04 -0500

    Update prune_neuron_cifar.py

--- prune_neuron_cifar.py
+++ prune_neuron_cifar.py
@@ -163,9 +163,9 @@ def evaluate_by_threshold(model, mask_values, pruning_max, pruning_step, criteri
         layer_name, neuron_idx, value = mask_values[idx][0], mask_values[idx][1], mask_values[idx][2]
         cl_loss, cl_acc = test(model=model, criterion=criterion, data_loader=clean_loader)
         po_loss, po_acc = test(model=model, criterion=criterion, data_loader=poison_loader)
-        print('{:.2f} \t {} \t {} \t {} \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f}'.format(
+        print('{:.2f} \t {} \t {} \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f}'.format(
             start, layer_name, neuron_idx, value, po_loss, po_acc, cl_loss, cl_acc))
-        results.append('{:.2f} \t {} \t {} \t {} \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f}\n'.format(
+        results.append('{:.2f} \t {} \t {} \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f}\n'.format(
             start, layer_name, neuron_idx, value, po_loss, po_acc, cl_loss, cl_acc))
     return results
 
